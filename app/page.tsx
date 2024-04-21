8ec25705d3d996961973ba3f4ee3de770bc4a40e
SeungYn <kwls0407@gmail.com>
2024-04-21 10:48:38 +0900

    feat: prisma 스키마 세팅

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Poppins } from 'next/font/google';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import LoginButton from '@/components/auth/LoginButton';
+import { db } from '@/lib/db';
 
 const font = Poppins({
   subsets: ['latin'],
