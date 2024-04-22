89b7ffe6c3200b54c6e66d196b729047b7ff75c2
SeungYn <kwls0407@gmail.com>
2024-04-23 02:30:05 +0900

    feat: authjs callback session token

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from 'next-auth';
 import authConfig from './auth.config';
 import { db } from './lib/db';
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { getUserById } from './data/user';
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +10,29 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ user, session, token }) {
+      console.log('session', { session, user, token });
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+
+      if (session.user && token.role) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token, user, account, profile, trigger }) {
+      // 토큰을 관리 토큰에 데이터를 추가해줄 수 있음, 그럼 세션에서 해당 토큰을 이용
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      console.log({ token, user, account, profile, trigger });
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   ...authConfig,
