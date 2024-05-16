e1928f1b96c914601fd4f343a9d960ddb681ab93
SeungYn <kwls0407@gmail.com>
2024-05-17 01:38:44 +0900

    feat: loginform code 추가

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -42,7 +42,7 @@ export default function LoginForm() {
     searchParams.get('error') === 'OAuthAccountNotLinked'
       ? '이메일은 이미 사용중입니다!'
       : '';
-
+  const [showTwoFactor, setShowTwoFactor] = useState(false);
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
   // startTransition 을 사용하면 revalidate관련 함수를 사용할 때 언제 끝나는지 알 수 있음
@@ -52,19 +52,35 @@ export default function LoginForm() {
     defaultValues: {
       email: '',
       password: '',
+      code: '',
     },
   });
-
+  console.log('login form');
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+    console.log('login submit');
     setError('');
     setSuccess('');
     // server action은 프로미스여도 스탈트랜지션이 적용됨
     startTransition(() => {
-      login(values).then((data) => {
-        setError(data?.error);
-        setSuccess(data?.success);
-        console.log(data);
-      });
+      login(values)
+        .then((data) => {
+          if (data?.error) {
+            form.reset();
+            setError(data.error);
+          }
+
+          if (data?.success) {
+            form.reset();
+            setSuccess(data.success);
+          }
+
+          if (data?.twoFactor) {
+            setShowTwoFactor(true);
+          }
+        })
+        .catch(() => {
+          setError('무엇인가 잘못됐습니다.');
+        });
       //test().then((data) => console.log(data));
     });
   };
@@ -79,55 +95,78 @@ export default function LoginForm() {
       <Form {...form}>
         <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
           <div className='space-y-4'>
-            <FormField
-              control={form.control}
-              name='email'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Email</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder='test@example.com'
-                      type='email'
-                      disabled={isPending}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            ></FormField>
-            <FormField
-              control={form.control}
-              name='password'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      placeholder='****'
-                      type='password'
-                      disabled={isPending}
-                    />
-                  </FormControl>
-                  <Button
-                    size='sm'
-                    variant='link'
-                    asChild
-                    className='px-0 font-nonal'
-                  >
-                    <Link href='/auth/reset'>Forgot Password?</Link>
-                  </Button>
-                  <FormMessage />
-                </FormItem>
-              )}
-            ></FormField>
+            {showTwoFactor && (
+              <FormField
+                control={form.control}
+                name='code'
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Two Factor Code</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder='123456'
+                        disabled={isPending}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              ></FormField>
+            )}
+            {!showTwoFactor && (
+              <>
+                <FormField
+                  control={form.control}
+                  name='email'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder='test@example.com'
+                          type='email'
+                          disabled={isPending}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                ></FormField>
+                <FormField
+                  control={form.control}
+                  name='password'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          placeholder='****'
+                          type='password'
+                          disabled={isPending}
+                        />
+                      </FormControl>
+                      <Button
+                        size='sm'
+                        variant='link'
+                        asChild
+                        className='px-0 font-nonal'
+                      >
+                        <Link href='/auth/reset'>Forgot Password?</Link>
+                      </Button>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                ></FormField>
+              </>
+            )}
           </div>
           <FormError message={error || urlError} />
           <FormSuccess message={success} />
           <Button type='submit' className='w-full' disabled={isPending}>
-            Login
+            {showTwoFactor ? '인증하기' : '로그인'}
           </Button>
         </form>
       </Form>
